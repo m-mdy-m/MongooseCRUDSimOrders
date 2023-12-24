1c09775bc04de24bb7fcf5e393e4f22fe653926d
mahdi <mahdimamashli1383@gmail.com>
2023-12-24 18:28:47 +0330

    Update product deletion route and method

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,13 @@
-const Product = require('../models/Product')
-exports.getDashboard = async (req,res)=>{
-    const products = await Product.find()
-    res.render('admin/dashboard', {
-        title : "dashboard",
-        path : req.path,
-        products
-    })
-}
-exports.deleteProds = async (req,res )=>{
-    const id = req.prams.prodId
-    console.log(id);
-}
+const Product = require("../models/Product");
+exports.getDashboard = async (req, res) => {
+  const products = await Product.find();
+  res.render("admin/dashboard", {
+    title: "dashboard",
+    path: req.path,
+    products,
+  });
+};
+exports.deleteProds = async (req, res) => {
+  const id = req.params.prodId;
+  console.log(id);
+};
