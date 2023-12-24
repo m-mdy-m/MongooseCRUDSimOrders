1268f0fcc48b44c197bee91d2314279ec4b624d7
mahdi <mahdimamashli1383@gmail.com>
2023-12-24 17:53:51 +0330

    Add product creation functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,11 @@
-exports.postAddProduct = (req,res,nxt)=>{
-    
+const Product = require('../models/Product')
+exports.postAddProduct = async (req,res,nxt)=>{
+    const  title = req.body.title
+    const price = req.body.price
+    const product = await Product.create({
+        title,
+        price
+    })
+    console.log('Create Product :', product);
+    return res.redirect('/')
 }
