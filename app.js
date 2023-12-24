a7902f82075e3d43efa8010aaa24b055aef6df17
mahdi <mahdimamashli1383@gmail.com>
2023-12-24 17:22:11 +0330

    Added Express, Mongoose, Body-parser, and EJS to the project.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(express.static(path.join(__dirname , "public")))
